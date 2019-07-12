7592a28f6817d8df0dcba6bda32d37e3c438aa0f
James Higgs <james.y.higgs@gmail.com>
2019-07-11 22:13:16 -0700

    2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,66 +1,48 @@
-'use strict';
+'use strict'
 
-const apiKey = /*your API key here*/
-
-const searchURL = 'https://newsapi.org/v2/everything';
-
-
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+function getRepos() {
+    const searchHandle = $("js-search-handle").val();
+    console.log(searchHandle);
+    fetch('https://api.github.com/users/' + searchHandle + '/repos')
+    .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      });
 }
 
-function displayResults(responseJson, maxResults) {
-  console.log(responseJson);
-  $('#results-list').empty();
-  for (let i = 0; i < responseJson.articles.length & i<maxResults ; i++){
-    $('#results-list').append(
-      `<li><h3><a href="${responseJson.articles[i].url}">${responseJson.articles[i].title}</a></h3>
-      <p>${responseJson.articles[i].source.name}</p>
-      <p>By ${responseJson.articles[i].author}</p>
-      <p>${responseJson.articles[i].description}</p>
-      <img src='${responseJson.articles[i].urlToImage}'>
-      </li>`
+function displayResults(responseJson) {
+    console.log(responseJson);
+    if (responseJson.message == 'not found') {
+        alert('Cannot find handle');
+    }
+    else (makeList(responseJson));
+    $('#results').removeClass('hidden')
+}
+
+function makeList(responseJson) {
+    $('#results-list').empty();
+    for (let i = 0; i < responseJson.length; i++) {
+        $('.results-list').append(
+            `<li><h3>${responseJson[i].name}</h3>
+            <h3><a href="${responseJson[i].url}">${responseJson[i].html_url}</a></h3>
+            </li>`
     )};
-  $('#results').removeClass('hidden');
-};
-
-function getNews(query, maxResults=10) {
-  const params = {
-    q: query,
-    language: "en",
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
-
-  console.log(url);
-
-  const options = {
-    headers: new Headers({
-      "X-Api-Key": apiKey})
-  };
-
-  fetch(url, options)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson, maxResults))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
 }
 
 function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getNews(searchTerm, maxResults);
+    $('form').submit(event => {
+      event.preventDefault();
+      getRepos();
+    });
+  }
+  
+  $(function() {
+    console.log('App works');
+    watchForm();
   });
-}
-
-$(watchForm);
