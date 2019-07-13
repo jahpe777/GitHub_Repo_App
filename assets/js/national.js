354c735673bae0165d7d2f21b76a43bc295041f7
James Higgs <james.y.higgs@gmail.com>
2019-07-13 13:49:21 -0700

    1

--- assets/js/national.js
+++ assets/js/national.js
@@ -1,41 +1,65 @@
-'use strict'
+'use strict';
 
-function getRepos() {
-    const searchHandle = $("#js-search-handle").val();
-    console.log(searchHandle);
-    fetch('https://api.github.com/users/' + searchHandle + '/repos')
-    .then(response => response.json())
-    .then(responseJson => displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again.'));
-};
+const apiKey = 'rHtS0gM9yKaoPQv2HSZonQ1LBR6k1axHLSSKU7LC';
 
-function displayResults(responseJson) {
-    console.log(responseJson);
-    if (responseJson.message == 'Not Found') {
-        alert('Cannot find handle');
-    }
-    else (makeList(responseJson));
-    $('.results').removeClass('hidden')
+const searchURL = 'https://developer.nps.gov/api/v1/parks/';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
 }
 
-function makeList(responseJson) {
-    $('#js-results-list').empty();
-    for (let i = 0; i < responseJson.length; i++) {
-        $('#js-results-list').append(
-            `<li><h3>${responseJson[i].name}</h3>
-            <h3><a href="${responseJson[i].html_url}">${responseJson[i].html_url}</a></h3>
-            </li>`
-    )};
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('#js-results-list').empty();
+  for (let i = 0; i < responseJson.data.length; i++){
+    $('#js-results-list').append(
+      `<li><h3>${responseJson.data[i].name}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <a href = "${responseJson.data[i].url}">${responseJson.data[i].url}</a>
+      </li>`
+    )};  
+  $('.results').removeClass('hidden');
+};
+
+function getParks(pickState, maxResults=10) {
+  const params = {
+    api_key: apiKey,
+    stateCode: pickState,
+    limit: maxResults
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchURL + '?' + queryString;
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => {
+        if(responseJson.total = 0) {
+          $("js-error-message").text("Parks not available. Try again.");  
+        } 
+        else {displayResults(responseJson)};
+    })
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 }
 
 function watchForm() {
-    $('form').submit(event => {
-      event.preventDefault();
-      getRepos();
-    });
-  }
-  
-  $(function() {
-    console.log('App works');
-    watchForm();
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchState = $('#js-search-state').val();
+    const pickState = searchState.replace(/\s+/g, '&statecode=');
+    const maxResults = $('#js-max-results').val();
+    getParks(pickState, maxResults);
   });
+}
+
+$(watchForm);
